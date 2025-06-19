098eac82c226d91c2c5e09e6e55e196435f03efb
Leonie THOMAS <leonie.thomas@uni.lu>
2025-06-19 16:32:41 +0200

    small change

---
+++ scr/convertmodelfrom_mat_xml.py
@@ -0,0 +1,7 @@
+from pathlib import Path
+from cobra.io import load_model,load_json_model, save_json_model, load_matlab_model, save_matlab_model, read_sbml_model, write_sbml_model
+import logging
+
+
+
+
