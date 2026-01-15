739b5d332c5ba08a99b7f75201cf533b9eddbc91
Vanille LEJAL <vanille.lejal@uni.lu>
2026-01-15 16:13:46 +0100

    Creating new main script and class for model_analysis step

---
+++ scr/no4_single_model_analysis.m
@@ -0,0 +1,15 @@
+%% Single model analysis script 
+% This script performs the analysis of a model, including:
+%   - FBA
+%   - FVA
+%   - flux_sum
+%   - sampling
+%   - single_gene_deletion
+%   - double_gene_deletion
+% The results of the performed analyses are stored in the project object.
+% A report presenting these results can be generated.
+% 
+%% PREPARATION OF THE ANALYSIS WORKFLOW
+%% LOADING CONTEXT SPECIFIC MODELS
+%% ANALYSIS
+%% REPORT
