fab81b1c78ddc944a72e754a7b507ad0901abe3a
Leonie THOMAS <leonie.thomas@uni.lu>
2026-04-08 09:48:08 +0200

    close the windows that are opened while executing the clustergramm function, giving back the output plot as object in project

--- scr/classes/models_comparison/functions/plot_clustergram.m
+++ scr/classes/models_comparison/functions/plot_clustergram.m
@@ -10,22 +10,25 @@ function [fig] = plot_clustergram(data,rownames, colnames,title,colorbarLabel,al
                       'Colormap', altcolor);  
            addTitle(cgo_J,title)
            cgf = plot(cgo_J); % This should be a figure handle
-           
+           % cgo_J is your clustergram object
+
            colorbar(cgf,'eastoutside');
 
            % Add colorbar and label
            cb = colorbar(cgf, 'eastoutside');
            cb.Label.String = colorbarLabel;
            % Set colorbar font size
-           cb.Label.FontSize = 16;
-           cb.FontSize = 16;  % tick labels        
+           cb.Label.FontSize = 14;
+           cb.FontSize = 14;  % tick labels        
 
            fig = gcf;
-           
+           fig.Visible = 'off';
            ax = findall(fig, 'Type', 'Axes');
            set(ax, 'FontSize', 16);
            txt = findall(fig, 'Type', 'Text');
            set(txt, 'FontSize', 16)
+           close(findall(0, 'Type', 'figure', 'Name', 'Clustergram 1'))
+           close(findall(0, 'Type', 'figure', 'Name', 'Clustergram 2'))
 
         end
     
