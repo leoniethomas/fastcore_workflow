c4e14e36a88e46c2a02f2fefb55e7ab3762271e1
Vanille LEJAL <vanille.lejal@uni.lu>
2026-08-05 12:55:51 +0200

    align with FVA being stored in proper struct and not just as a table anymore

--- scr/classes/models_comparison/functionsReorganized/computeFvaSimilarity.m
+++ scr/classes/models_comparison/functionsReorganized/computeFvaSimilarity.m
@@ -1,24 +1,23 @@
-function [fva_similarity,fva_similarity_rxns,fva_similarity_pathways] = computeFvaSimilarity(project,comparison)
+function [fvaSimilarity, fvaSimilarityRxns, fvaSimilarityPathways] = computeFvaSimilarity(project, comparison)
             
             modelList = project.comparisons.(comparison).modelNames;
             referenceModel = project.comparisons.(comparison).referenceModel;
 
-            replacement_value = "analysis.active.FVA.maxFlux"; % get the fba solution values
-            ordered_fva_max_matrix = getOrderedFeatureMatrix(project,modelList,"rxns",referenceModel,replacement_value);
-            replacement_value = "analysis.active.FVA.minFlux"; % get the fba solution values
-            [ordered_fva_min_matrix,rxn_mapping] = getOrderedFeatureMatrix(project,modelList,"rxns",referenceModel,replacement_value);
+            replacementValue = "analysis.active.FVA.minMaxFluxes.maxFlux";
+            orderedFvaMaxMatrix = getOrderedFeatureMatrix(project, modelList, referenceModel, "rxns", replacementValue);
+            replacementValue = "analysis.active.FVA.minMaxFluxes.minFlux";
+            [orderedFvaMinMatrix, rxnMapping] = getOrderedFeatureMatrix(project, modelList, referenceModel, "rxns", replacementValue);
             
             n = numel(modelList);
 
-            fva_similarity = eye(n); % Diagonal is 1
-            fva_similarity_rxns = cell(n);
+            fvaSimilarity = eye(n); % Diagonal is 1
+            fvaSimilarityRxns = cell(n);
             
             fvaData = cell(1, n);
             for i = 1:n
-                fvaData{i} = [ordered_fva_min_matrix(:, i), ordered_fva_max_matrix(:, i)];
+                fvaData{i} = [orderedFvaMinMatrix(:, i), orderedFvaMaxMatrix(:, i)];
             end
 
-            
             indexPairs = find(triu(true(n), 1)); % Upper triangle linear indices
             [rowIdx, colIdx] = ind2sub([n, n], indexPairs);
 
@@ -31,45 +30,44 @@ function [fva_similarity,fva_similarity_rxns,fva_similarity_pathways] = computeF
                 % filter out reactions that are not in both of the models
                 % the differences on structural level are not interesting
                 % in this figure
-                rxnidx_in_both_models = find(sum(rxn_mapping(:,[x,y])~= 0,2) == 2);
-                overallSim = mean(rxnSim(rxnidx_in_both_models,:));
+                rxnIdxInBothModels = find(sum(rxnMapping(:, [x,y]) ~= 0, 2) == 2);
+                overallSim = mean(rxnSim(rxnIdxInBothModels, :));
 
-                % Fill both [y,x] and [x,y] due to symmetry
-                fva_similarity(y,x) = overallSim;
-                fva_similarity(x,y) = overallSim;
+                % Fill both [y, x] and [x, y] due to symmetry
+                fvaSimilarity(y, x) = overallSim;
+                fvaSimilarity(x, y) = overallSim;
 
-                fva_similarity_rxns{y,x} = rxnSim;
-                fva_similarity_rxns{x,y} = rxnSim;
+                fvaSimilarityRxns{y, x} = rxnSim;
+                fvaSimilarityRxns{x, y} = rxnSim;
             end
 
-            n_models = size(fva_similarity_rxns,2);
+            nModels = size(fvaSimilarityRxns, 2);
 
-            fva_similarity_pathways = cell(n_models);
-        
+            fvaSimilarityPathways = cell(nModels);
             
             pathways = string(project.models.(referenceModel).model.subSystems); % get pathways from reference model
-            unique_pathways = unique(pathways); 
+            uniquePathways = unique(pathways); 
         
             % for every pathway get the matrix identifying the rnxs from reference
             % model in this pathway
-            groups = arrayfun(@(x) find(pathways == x), unique_pathways, 'UniformOutput', false);
-            num_groups = length(groups);
+            groups = arrayfun(@(x) find(pathways == x), uniquePathways, 'UniformOutput', false);
+            numGroups = length(groups);
         
-            indexPairs = find(triu(true(n_models), 1)); % Upper triangle linear indices
-            [rowIdx, colIdx] = ind2sub([n_models, n_models], indexPairs);
+            indexPairs = find(triu(true(nModels), 1)); % Upper triangle linear indices
+            [rowIdx, colIdx] = ind2sub([nModels, nModels], indexPairs);
         
             for k = 1:length(indexPairs)
                         y = rowIdx(k);
                         x = colIdx(k);
         
-                        matrix_fva_rxns = fva_similarity_rxns{y,x};
+                        matrixFvaRxns = fvaSimilarityRxns{y, x};
                         G = {};
                     
-                        for g = 1:num_groups
-                            G{g} = matrix_fva_rxns(intersect(rxnidx_in_both_models,groups{g}),1);
+                        for g = 1:numGroups
+                            G{g} = matrixFvaRxns(intersect(rxnIdxInBothModels, groups{g}), 1);
                         end
-                        fva_similarity_pathways{y,x} = cellfun(@mean, G);
-                        fva_similarity_pathways{x,y} = cellfun(@mean, G);
+                        fvaSimilarityPathways{y, x} = cellfun(@mean, G);
+                        fvaSimilarityPathways{x, y} = cellfun(@mean, G);
             end
 end
 
