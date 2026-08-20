817587107a2096d3485f447f29b62095e33f899a
Leonie THOMAS <leonie.thomas@uni.lu>
2026-08-20 18:26:57 +0100

    checking the changes to getFluxsum.m regarding only computing the fluxsum on the full matrix when needed, improves computation time

--- scr/classes/models_comparison/functionsReorganized/getFluxsum.m
+++ scr/classes/models_comparison/functionsReorganized/getFluxsum.m
@@ -15,10 +15,6 @@ function fluxsumCell = getFluxsum(project, comparisonName, metIdx, rxnIdx, slot,
     if isempty(rxnIdx)
         rxnIdx = {find(ones(length(project.models.(reference).model.rxns), 1))};
     end
-
-    % if isempty(metIdx)
-    %         metIdx = find(ones(length(project.models.(reference).model.mets), 1));
-    % end
     
     if fluxSummedUp == "reactions"
         % No metabolite dimension, full S
@@ -32,17 +28,5 @@ function fluxsumCell = getFluxsum(project, comparisonName, metIdx, rxnIdx, slot,
         fluxsumCell = cellfun(@(x) getModelFluxsum(fullS(metIdx, x), samples(x, :), fluxSummedUp), ...
             rxnIdx, 'UniformOutput', false); % faster than calculating on full S and then filtering
     end
-
-    % fluxsumCell = cellfun(@(x) getModelFluxsum(fullS(:, x), samples(x, :), fluxSummedUp), ...
-    %                        rxnIdx, 'UniformOutput', false);
-    
-    % if fluxSummedUp ~= "reactions"
-    %     % this is only needed in case the fluxsum is computed over all the
-    %     % metabolites in the given reactions, as an additional filtering
-    %     % step, in case of "reactions" only one value is returend per set
-    %     % for the getModelFluxsum
-    %     fluxsumCell = cellfun(@(f) f(metIdx, :), ...
-    %                            fluxsumCell, 'UniformOutput', false);
-    % end
 
 end
