cac2ffef759d40afc07bde767c3fa6dae5e66929
Leonie THOMAS <leonie.thomas@uni.lu>
2026-08-24 21:59:56 +0100

    correct active analysis function

--- scr/classes/models_comparison/functionsReorganized/getActiveAnalysisIDTable.m
+++ scr/classes/models_comparison/functionsReorganized/getActiveAnalysisIDTable.m
@@ -5,7 +5,13 @@ function activeAnalysisTable = getActiveAnalysisIDTable(project,modelList)
     modelNames     = string(fieldnames(filteredModels));
     
     % get analysis fields excluding parameters
-    analysisFields = setdiff(fieldnames(structfun(@(x) x.analysis.active, filteredModels, 'UniformOutput', true)), 'parameters');
+    analysisInModels = structfun(@(x) fieldnames(x.analysis.active), filteredModels, 'UniformOutput', false);
+    C = struct2cell(analysisInModels);
+    analysisFields = C{1};
+    for k = 2:numel(C)
+        analysisFields = intersect(analysisFields, C{k});
+    end
+    analysisFields = setdiff(analysisFields,'parameters');
     
     % build table
     activeAnalysisTable = struct();
